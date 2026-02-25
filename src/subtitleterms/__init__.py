c7153e7da4b696799e4240bd57f7fb00926f4d70
Derek Enlow <derekenlow@gmail.com>
2026-02-24 20:25:29 -0700

    beginning integration

--- src/subtitleterms/__init__.py
+++ src/subtitleterms/__init__.py
@@ -1,34 +1,47 @@
-# import the main window object (mw) from aqt
 import os
 import sys
 
 from aqt import mw
 from aqt.qt import QAction
-
-# import the "show info" tool from utils.py
-from aqt.utils import qconnect
+from aqt.utils import qconnect, showWarning
 
 parent_dir = os.path.abspath(os.path.dirname(__file__))
 vendor_dir = os.path.join(parent_dir, "vendor")
 sys.path.append(vendor_dir)
 
+from .deck import builders  # noqa: E402
 from .ui import ImportDialog  # noqa: E402
-
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
+from .ext import ext, parse_srt  # noqa: E402
 
 
-def testFunction() -> None:
+def importDeck() -> None:
     # get the number of cards in the current collection, which is stored in
     # the main window
     # cardCount = mw.col.card_count()
     # show a message box
     # showInfo("Card count: %d" % cardCount)
-    ImportDialog().getSettings()
+    import_settings = ImportDialog().getSettings()
+    if not import_settings:
+        return
+    builder = builders[import_settings.deck]
+    if import_settings.subtitle_stream > -1:
+        sub_text = ext(import_settings.path, import_settings.subtitle_stream)
+        subs = parse_srt(sub_text)
+    else:
+        # TODO: Better determine the encoding.
+        # TODO: utf-8-sig instead?
+        sub_text = import_settings.path.read_text(encoding="utf-8")
+        if import_settings.path.suffix == ".srt":
+            subs = parse_srt(sub_text)
+        else:
+            subs = sub_text.split("\n\r")
+
+    showWarning("".join(subs[:3]))
+    # builder.build(subs)
 
 
 # create a new menu item, "test"
 action = QAction("AnkiSub: Import from Subbed", mw)
 # set it to call testFunction when it's clicked
-qconnect(action.triggered, testFunction)
+qconnect(action.triggered, importDeck)
 mw.form.menuCol.insertAction(mw.form.menuCol.actions()[-1], action)
