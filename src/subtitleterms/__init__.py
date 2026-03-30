f8e2d2b0ed0f9e26cb312ea48cfe850d1a0060a4
Derek Enlow <derekenlow@gmail.com>
2026-03-29 22:02:01 -0600

    Avoid pytest disruptions from missing Qt windows.

--- src/subtitleterms/__init__.py
+++ src/subtitleterms/__init__.py
@@ -13,24 +13,28 @@ sys.path.append(vendor_dir)
 from .actions import importDeck, updateModels, updateNotes  # noqa: E402
 from .i18n import localization  # noqa: E402
 
-# Configure logger.
-logger = AddonManager.get_logger("subtitleterms")
-LOGLEVEL = os.environ.get("SUBTERMS_LOGLEVEL", "").upper()
-if LOGLEVEL.isdigit():
-    logger.setLevel(int(LOGLEVEL))
-elif LOGLEVEL:
-    logger.setLevel(LOGLEVEL)
-logger.info(f"SubtitleTerms log level is {logger.getEffectiveLevel()}")
+# Don't try to set up if running tests.
+if "pytest" not in sys.modules:
+    # Configure logger.
+    logger = AddonManager.get_logger("subtitleterms")
+    LOGLEVEL = os.environ.get("SUBTERMS_LOGLEVEL", "").upper()
+    if LOGLEVEL.isdigit():
+        logger.setLevel(int(LOGLEVEL))
+    elif LOGLEVEL:
+        logger.setLevel(LOGLEVEL)
+    logger.info(f"SubtitleTerms log level is {logger.getEffectiveLevel()}")
 
-# Create menu actions.
-actionImport = QAction(f"SubtitleTerms: {localization['toolbar_import']}", mw)
-qconnect(actionImport.triggered, importDeck)
-mw.form.menuCol.insertAction(mw.form.menuCol.actions()[-1], actionImport)
+    # Create menu actions.
+    actionImport = QAction(f"SubtitleTerms: {localization['toolbar_import']}", mw)
+    qconnect(actionImport.triggered, importDeck)
+    mw.form.menuCol.insertAction(mw.form.menuCol.actions()[-1], actionImport)
 
-actionModels = QAction(f"SubtitleTerms: {localization['toolbar_update_models']}", mw)
-qconnect(actionModels.triggered, updateModels)
-mw.form.menuTools.addAction(actionModels)
+    actionModels = QAction(
+        f"SubtitleTerms: {localization['toolbar_update_models']}", mw
+    )
+    qconnect(actionModels.triggered, updateModels)
+    mw.form.menuTools.addAction(actionModels)
 
-actionNotes = QAction(f"SubtitleTerms: {localization['toolbar_update_notes']}")
-qconnect(actionNotes.triggered, updateNotes)
-mw.form.menuTools.addAction(actionNotes)
+    actionNotes = QAction(f"SubtitleTerms: {localization['toolbar_update_notes']}")
+    qconnect(actionNotes.triggered, updateNotes)
+    mw.form.menuTools.addAction(actionNotes)
