443d61d29273aaa6203cd0969a2054de57cd9010
Derek Enlow <derekenlow@gmail.com>
2026-06-11 19:45:45 -0600

    Safer cleanup and tar to zip.

--- vendors.py
+++ vendors.py
@@ -1,6 +1,8 @@
-from shutil import rmtree
-import subprocess
 import shutil
+import subprocess
+import tarfile
+from pathlib import Path
+
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 
@@ -12,9 +14,7 @@ class CustomBuildHook(BuildHookInterface):
         self.install_vendors()
 
     def finalize(self, version, build_data, artifact_path):
-        # TODO: Convert to zip, and (optionally?) extract to a given path,
-        # to be linked as an Anki plugin.
-        pass
+        self.tar_to_zip(artifact_path)
 
     def install_vendors(self):
         dependencies = self.metadata.core.dependencies
@@ -28,5 +28,30 @@ class CustomBuildHook(BuildHookInterface):
         subprocess.run(uv_args)
 
     def uninstall_vendors(self):
-        vendor_dir = self.config["vendor-directory"]
-        rmtree(vendor_dir)
+        vendor_dir = Path(self.config["vendor-directory"])
+        if not vendor_dir.is_absolute():
+            vendor_dir = Path(self.root).joinpath(vendor_dir)
+        else:
+            try:
+                vendor_dir.relative_to(self.root)
+            except ValueError:
+                return
+
+        if vendor_dir.is_dir() and vendor_dir.exists():
+            shutil.rmtree(vendor_dir)
+
+    def tar_to_zip(self, path_str):
+        path = Path(path_str)
+        if self.target_name == "sdist" and path.name.endswith(".tar.gz"):
+            with tarfile.open(path) as tar:
+                tar.extractall(path.parent, filter="data")
+            artifact_name = path.name[: -len(".tar.gz")]
+            extracted_to = path.parent.joinpath(artifact_name + "/")
+            # print(artifact_name)
+            # print(str(path)[: len(".tar.gz")])
+            if extracted_to.exists() and extracted_to.is_dir():
+                shutil.make_archive(
+                    str(path.with_name(artifact_name)),
+                    "zip",
+                    extracted_to,
+                )
